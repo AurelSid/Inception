5693b6390dc2b2988a2d58eae401e0633f4d6a67
Your Name <example@gmail.com>
2025-03-10 18:15:36 +0100

    s

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
+sleep 10
+
 chown -R www-data /var/www/wordpress
 chmod -R 775 /var/www/wordpress
 
 mkdir -p /run/php/
 touch /run/php/php7.3-fpm.pid
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+if [ ! -f /usr/local/bin/wp ]; then
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
 
 cd /var/www/wordpress
 
-wp core download --allow-root
-
-mv /var/www/wp-config.php /var/www/wordpress/
-
-wp core install --allow-root --url=${DOMAIN_NAME} --title=${WORDPRESS_NAME} --admin_user=${WORDPRESS_ROOT_LOGIN} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=${WORDPRESS_ROOT_EMAIL}
-wp user create ${MYSQL_USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MYSQL_PASSWORD} --role=author --allow-root
-wp theme install inspiro --activate --allow-root
-wp plugin update --all --allow-root
-wp option update siteurl "https://asideris.42.fr" --allow-root
-wp option update home "https://asideris.42.fr" --allow-root
-
-
-
+if [ ! -d /var/www/wordpress/wp-admin ]; then
+    wp core download --allow-root
 fi
 
-/usr/sbin/php-fpm7.3 -F
+if [ ! -f wp-config.php ]; then
+    wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} \
+        --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root --skip-check
+fi
+
+if [ -f wp-config.php ]; then
+    wp core install --allow-root \
+        --url=${DOMAIN_NAME} \
+        --title=${WORDPRESS_NAME} \
+        --admin_user=${WORDPRESS_ROOT_LOGIN} \
+        --admin_password=${MYSQL_ROOT_PASSWORD} \
+        --admin_email=${WORDPRESS_ROOT_EMAIL}
+
+    wp user create ${USER} ${WORDPRESS_USER_EMAIL} --user_pass=${MYSQL_PASSWORD} --role=author --allow-root
+
+    wp config set WP_DEBUG ${WP_DEBUG} --allow-root
+
+    wp config set FORCE_SSL_ADMIN 'false' --allow-root
+
+    chmod 777 /var/www/wordpress/wp-content
+
+    wp theme install twentyfifteen --allow-root
+    wp theme activate twentyfifteen --allow-root
+    wp theme update twentyfifteen --allow-root
+fi
+
+exec /usr/sbin/php-fpm7.3 -F
